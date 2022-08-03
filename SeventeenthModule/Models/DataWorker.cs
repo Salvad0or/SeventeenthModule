8c572f7d19dbc9ca664479938c314ab1d0776402
- <wavevape@gmail.com>
2022-08-03 19:56:09 +0300

    Add project files.

---
+++ SeventeenthModule/Models/DataWorker.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeventeenthModule.Models
+{
+    internal class DataWorker
+    {
+        SqlConnectionStringBuilder ConnectionString { get; set; }
+
+        public DataWorker()
+        {
+            ConnectionString = new SqlConnectionStringBuilder
+            {
+                DataSource = @"(LocalDB)\MSSQLLocalDB",
+                InitialCatalog = "ITVDN2db",
+                Pooling = true
+            };
+
+            SqlConnection sql = new SqlConnection(ConnectionString.ToString());
+
+            sql.Open();
+
+            sql.Close();
+        }
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Саша\ITVDN2db.mdf;Integrated Security=True;Connect Timeout=30
+    }
+}
