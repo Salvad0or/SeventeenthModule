bace57fca062836f7ed08a75e344de9b3d1e9906
- <wavevape@gmail.com>
2022-08-05 22:30:04 +0300

    Реализована ViewModel

---
+++ SeventeenthModule/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SeventeenthModule.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeventeenthModule.ViewModel
+{
+    internal class MainWindowViewModel : ViewModels
+    {
+
+    }
+}
